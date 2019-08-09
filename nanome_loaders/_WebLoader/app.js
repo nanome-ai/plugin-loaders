f585de27d9dd358695a386df58630d8c76588cae
Alex McNerney <ajm13@me.com>
2019-08-09 10:34:50 -0700

    Updated WebUI

---
+++ nanome_loaders/_WebLoader/app.js
@@ -0,0 +1,87 @@
+'use strict'
+
+new Vue({
+  el: '#app',
+
+  data: {
+    files: [],
+    numDropping: 0,
+    isDroppable: false,
+    isUploading: false,
+    validExt: ['.pdb', '.sdf', '.cif', '.ppt', '.pptx', '.pdf']
+  },
+
+  created() {
+    document.body.addEventListener('dragenter', e => {
+      this.numDropping = e.dataTransfer.items.length
+      this.isDroppable = true
+    })
+
+    this.requestList()
+  },
+
+  methods: {
+    async requestList() {
+      const res = await fetch('//localhost:8081/list')
+      const json = await res.json()
+      const files = json.file_list.map(name => ({ name }))
+
+      this.files = []
+      for (const file of files) {
+        const [full, name, ext] = /^(.+)\.(\w+)$/.exec(file.name)
+        this.files.push({ full, name, ext })
+      }
+    },
+
+    async deleteFile(filename) {
+      // const index = this.files.findIndex(f => f.full == filename)
+      // this.files.splice(index, 1)
+
+      await fetch('//localhost:8081', {
+        method: 'DELETE',
+        data: filename
+      })
+
+      await this.requestList()
+    },
+
+    onChange(e) {
+      this.upload(e.target.files)
+      e.target.value = null
+    },
+
+    drop(e) {
+      const files = e.dataTransfer.files
+      this.upload(files)
+      this.isDroppable = false
+    },
+
+    async upload(files) {
+      this.isUploading = true
+
+      const numBefore = files.length
+      files = [...files].filter(f => {
+        const [, ext] = /(\.\w+)$/.exec(f.name)
+        return this.validExt.includes(ext)
+      })
+
+      const numRemoved = numBefore - files.length
+      if (numRemoved) {
+        alert(`${numRemoved} unsupported files skipped`)
+      }
+
+      const data = new FormData()
+      for (const file of files) {
+        data.append('files[]', file)
+      }
+
+      await fetch('//localhost:8081/', {
+        method: 'POST',
+        body: data
+      })
+      await this.requestList()
+
+      this.isUploading = false
+    }
+  }
+})
