f6ad553ec7fe49b4b52baab446b6efa314a7506d
Ramji <jeremie.rahm@epitech.eu>
2019-08-13 08:58:13 -0700

    Bump version: 0.3.3 → 0.3.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (pathlib.Path(__file__).parent / "README.md").read_text()
 setup(
 	name = 'nanome-loaders',
 	packages=find_packages(),
-	version = '0.3.3',
+	version = '0.3.4',
 	license='MIT',
 	description = 'Nanome Plugin deploying a few molecle loaders integrated with Nanome',
 	long_description = README,
