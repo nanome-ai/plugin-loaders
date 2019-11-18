d8fcc471797392a03eddcb23bdbcb149db468d07
Alex McNerney <ajm13@me.com>
2019-11-18 14:31:10 -0800

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (pathlib.Path(__file__).parent / "README.md").read_text()
 setup(
 	name = 'nanome-loaders',
 	packages=find_packages(),
-	version = '1.0.2',
+	version = '1.0.3',
 	license='MIT',
 	description = 'Nanome Plugin deploying a few molecule loaders integrated with Nanome',
 	long_description = README,
